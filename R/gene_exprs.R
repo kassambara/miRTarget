068bae626f4a12740b8aa0890439de9867abbf68
Alboukadel Kassambara <alboukadel.kassambara@gmail.com>
2016-08-11 06:55:19 -0400

    gene_exprs data added

---
+++ R/gene_exprs.R
@@ -0,0 +1,12 @@
+#' Expression value of miRNA potential targets
+#'
+#' @description Expression value of miR-28-5p and miR-768-5p potential targets
+#' @name gene_exprs
+#' @docType data
+#' @usage data("gene_exprs")
+#' @format A data frame with 94 rows and 4 columns (sample 1 to 4).
+#' @examples
+#' data(gene_exprs)
+#' head(gene_exprs)
+#'
+NULL
